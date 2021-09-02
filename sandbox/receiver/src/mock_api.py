1f5e06bbe7f06db74728b8b26f116d94091b8ea7
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-09-02 13:56:06 +0100

    aMB-827 lint fix]

--- sandbox/receiver/src/mock_api.py
+++ sandbox/receiver/src/mock_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, Response
 import os
 
 app = Flask(__name__)
@@ -6,16 +6,13 @@ app = Flask(__name__)
 
 @app.route("/meta")
 def meta():
-    return Response(
-        status=200
-    )
+    return Response(status=200)
 
 
 @app.route("/_status")
 def status():
-    return Response(
-        status=200
-    )
+    return Response(status=200)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
