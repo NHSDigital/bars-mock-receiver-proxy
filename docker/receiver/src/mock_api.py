c350fccc455bbe9a42893d76cad91a0c601f6561
Matthew Clark <matthew.clark21@nhs.net>
2021-09-08 15:18:39 +0100

    AMB-813 added 200 response for /slots

--- docker/receiver/src/mock_api.py
+++ docker/receiver/src/mock_api.py
@@ -13,5 +13,11 @@ def status():
     return Response(status=200)
 
 
+@app.route("/slots")
+def slots():
+    return Response(status=200)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
+
