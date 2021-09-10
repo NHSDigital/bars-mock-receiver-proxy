58ea1465c1b6e41d0f85497b7729b5ec07166b41
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-09-10 15:38:00 +0100

    AMB-813 add 400 error for missing start query

--- docker/receiver/src/routes/slots.py
+++ docker/receiver/src/routes/slots.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Request, Response
 from .examples.example_loader import load_example
 
 slots = Blueprint('slots', __name__)
@@ -6,4 +6,9 @@ slots = Blueprint('slots', __name__)
 
 @slots.route('/Slots', methods=['GET'])
 def get_slots():
+    if "start" not in Request.args:
+        status = 400
+    
+    if status == 400:
+        return Response('Bad Request', status)
     return load_example('slots/GET-success.json')
