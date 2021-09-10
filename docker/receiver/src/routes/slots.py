d538ff1812393916970faa5e02d9d3435d7a71bf
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-09-10 16:02:42 +0100

    AMB-813 add required query params to Slots

--- docker/receiver/src/routes/slots.py
+++ docker/receiver/src/routes/slots.py
@@ -6,7 +6,16 @@ slots = Blueprint('slots', __name__)
 
 @slots.route('/Slots', methods=['GET'])
 def get_slots():
+    status = 200
+    if "healthcareService" not in request.args:
+        status = 400
+    if "status" not in request.args:
+        status = 400
     if "start" not in request.args:
+        status = 400
+    if "end" not in request.args:
+        status = 400
+    if "_include" not in request.args:
         status = 400
 
     if status == 400:
