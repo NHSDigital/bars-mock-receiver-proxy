fd3637887755dc060a7f54dcde2d9039d7fe476c
Valentinolucas <valentinolucasfantini@gmail.com>
2021-09-13 16:12:59 +0100

    AMB-813 try fastapi

---
+++ docker/receiver/src/main.py
@@ -0,0 +1,29 @@
+import uvicorn
+
+from typing import Optional
+from fastapi import FastAPI, Query, HTTPException
+from datetime import datetime
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from enum import Enum
+
+
+class Status(Enum):
+    status_free = "free"
+    status_busy = "busy"
+
+
+app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request, exc):
+    return PlainTextResponse(str("something"), status_code=400)
+
+@app.get("/Slot")
+def Slot(healthcareService: str, status: Status, start: datetime, end: datetime, _include: str):
+    return {"healthcareService": healthcareService}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
