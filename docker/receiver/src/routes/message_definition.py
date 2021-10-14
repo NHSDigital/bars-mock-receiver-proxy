5e8cfe3260f2cfb2cdb8c94a497467d2e59ea29a
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-10-14 14:53:41 +0100

    AMB-862 add MessageDefinition endpoint

--- docker/receiver/src/routes/message_definition.py
+++ docker/receiver/src/routes/message_definition.py
@@ -4,6 +4,6 @@ from .example_loader import load_example
 route = APIRouter()
 
 
-@route.get("/metadata")
-def get_metadata(NHSD_Service: str = Header(...), NHSD_Token: str = Header(...)):
-    return load_example("metadata/GET-success.json")
+@route.get("/MessageDefinition")
+def get_metadata(NHSD_Service: str = Header(...)):
+    return load_example("message_definition/GET-success.json")
