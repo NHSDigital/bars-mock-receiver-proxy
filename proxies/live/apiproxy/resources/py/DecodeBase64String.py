bc6f77871c203f25baac7902b083f8995f06ab79
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2022-04-21 09:39:30 +0100

    AMB-000 set decoded header

--- proxies/live/apiproxy/resources/py/DecodeBase64String.py
+++ proxies/live/apiproxy/resources/py/DecodeBase64String.py
@@ -3,4 +3,4 @@ import base64
 b64encoded = flow.getVariable("request.header.NHSD-Target-Identifier")
 b64decoded = base64.b64decode(b64encoded)
 
-flow.setVariable("request.header.NHSD-Target-Identifier", b64decoded)
+flow.setVariable("b64decoded", b64decoded)
